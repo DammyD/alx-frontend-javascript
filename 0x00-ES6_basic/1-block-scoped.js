8143bfe794508e4ae9a1f7b14fb10eb72fbd5dac
DammyD <ibukunoluwadawodu@gmail.com>
2023-01-23 20:02:55 +0000

    :sparkles: Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,6 +2,9 @@ export default function taskBlock(trueOrFalse) {
   var task;
   var task2;
 
+  var task = false;
+  var task2 = true;
+
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
