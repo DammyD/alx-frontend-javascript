157576625b80737a910c32e1454edd64eba3d127
DammyD <ibukunoluwadawodu@gmail.com>
2023-01-23 23:18:01 +0000

    :wrench: Task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arr = [];	
-  for (const idx of array){
-    const value = idx;	  
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
     arr.push(appendString + value);
   }
 
